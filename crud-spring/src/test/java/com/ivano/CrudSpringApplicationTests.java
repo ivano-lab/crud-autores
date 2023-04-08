7026234e5600884fc463bef04c9d51d4143a1bd9
Ívano Fontes <ivanofnts@gmail.com>
2023-04-08 01:41:36 -0300

    spring time

---
+++ crud-spring/src/test/java/com/ivano/CrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
